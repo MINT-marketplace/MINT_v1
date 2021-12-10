125279698f0baa92f70a0c383449cbbb5faeb31f
MINT <20200863@iade.pt>
2021-12-10 22:10:06 +0000

    querys commit

--- src/main/java/pt/iade/mint/models/Repositories/UtilizadorRepository.java
+++ src/main/java/pt/iade/mint/models/Repositories/UtilizadorRepository.java
@@ -1,7 +1,9 @@
 package pt.iade.mint.models.Repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import pt.iade.mint.models.Utilizador;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UtilizadorRepository extends CrudRepository<Utilizador, Integer> {
+
 }
